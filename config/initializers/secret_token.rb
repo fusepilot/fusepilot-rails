404bd4b249590630ca15b7b0dee429bf3c6fe5dc
Michael Delaney <michaeldelaney@Michaels-Mac-Pro.local>
2011-11-12 13:14:10 -0600

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FusepilotRails::Application.config.secret_token = '97396d8f77f833e3f9f540c8ff644cc21f8c17e3af475d7d5e8766d680ab7d84c74cc86deffeb33d235e8f554d9bfc3d75f366b0ba6decd7c88992df1616276c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FusepilotRails::Application.config.session_store :cookie_store, key: '_fusepilot_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FusepilotRails::Application.config.session_store :active_record_store
